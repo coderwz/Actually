0b081f8f685ab85d029740d1c0d04d471db6c2b4
sirius0804 <yimingjia@yahoo.com>
2014-10-30 23:40:28 -0400

    Yiming Jia

--- actually/githubActually/models.py
+++ actually/githubActually/models.py
@@ -4,6 +4,10 @@ from django.utils import timezone
 
 # Create your models here.
 # Users include developers and product managers
+# give github name , return projects
+# give project , return all users, and pm( at top of list)
+# give a github name, return all milestones 
+
 class Project(models.Model):
 	name = models.CharField(max_length = 100)
 	description = models.CharField(max_length = 1000, null = True, blank = True)
@@ -12,11 +16,6 @@ class Project(models.Model):
 	progress = models.FloatField(default = 0)
 	prevProgress =models.FloatField(default = 0)
 
-	# def __init__(self, name, description, startTime):
-	# 	self.name = name
-	# 	self.description = description
-	# 	self.startTime = startTime
-	# 	self.progress = 0
 
 	def __unicode__(self):
 		return self.name
@@ -25,6 +24,9 @@ class Project(models.Model):
 		self.prevProgress = self.progress
 		self.progress = progress
 
+	def findPmDeveloperByProject(self,GivenProject):
+		findPM = Project.objects.filter(name = GivenProject)
+		return 
 
 
 class PM(models.Model):
@@ -36,10 +38,7 @@ class PM(models.Model):
 	def __unicode__(self):
 		return '{} {}'.format(self.firstName, self.lastName)
 
-	def __init__(self, name, githubName, project):
-		self.name = name
-		self.project = project
-		self.githubName = githubName
+
 
 class Developer(models.Model):
 	firstName = models.CharField(max_length = 50)
@@ -48,15 +47,10 @@ class Developer(models.Model):
 	pmAssigned = models.ForeignKey(PM, null = True, blank = True)
 	project = models.ManyToManyField(Project, null = True, blank = True)
 
-	def __init__(self, name, githubName, pmAssigned, project):
-		self.name = name
-		self.pmAssigned = pmAssigned
-		self.project = project
-		self.githubName = githubName
-
-
-
-
+	def findProjectByUser(self, GivenGithub_name):
+		#many to many
+		findDevloper = Project.objects.filter(githubName=GivenGithub_name)
+		return findDevloper
 
 class Milestone(models.Model):
 	name = models.CharField(max_length = 100)
@@ -66,16 +60,8 @@ class Milestone(models.Model):
 	percentage = models.FloatField()
 	dueDate = models.DateTimeField()
 
-	def __init__(self, name, description, dueDate, percentage):
-		self.name = name
-		self.description = description
-		self.dueDate = datetime.now()
-		self.progress = 0
-		# Section.objects.filter(milestone = self)
-
 	def __unicode__(self):
 		return self.name
-
 
 	def update(self, progress):
 		self.prevProgress = self.progress
@@ -84,10 +70,6 @@ class Milestone(models.Model):
 		for sec in section:
 			prog = prog + sec.percentage
 		self.progress = self.progress + progress/prog
-
-
-
-
 
 class Section(models.Model):
 	name = models.CharField(max_length = 100)
@@ -99,16 +81,6 @@ class Section(models.Model):
 	project = models.ForeignKey(Project)
 	milestone = models.ForeignKey(Milestone)
 
-	# def __init__(slef, name, description, percentage, project, progress, developer, milestone):
-	# 	self.name = name
-	# 	self.description = description
-	# 	self.percentage = percentage
-	# 	self.progress = 0
-	# 	self.project = project
-	# 	self.developer = developer
-	# 	self.milestone = milestone
-
-
 	def __unicode__(self):
 		return self.name
 
@@ -117,11 +89,6 @@ class Section(models.Model):
 		self.progress = self.progress + progress
 		progressUpdate = self.percentage * progress 
 		self.project.update(progressUpdate)
-		# self.milestone.update(progressUpdate)
-
-	# def search_finished_section():
-	# 	pass
-
 
 class Task(models.Model):
 	name = models.CharField(max_length = 100)
@@ -137,35 +104,14 @@ class Task(models.Model):
 		updateProgress = self.percentage * (self.progress - self.prevProgress)
 		self.section.update(updateProgress)
 
-	# def __init__(self, name, description, percentage, section):
-	# 	self.name = name
-	# 	self.description = description
-	# 	self.percentage = percentage
-	# 	self.progress = 0
-	# 	self.section = section
-
 	def __unicode__(self):
 		return self.name
-
-
-
-
-
-
-
-
 
 class Commit(models.Model):
 	commitTime = models.DateTimeField()
 	progress = models.FloatField()
 	developer = models.ForeignKey(Developer)
 	task = models.ForeignKey(Task)
-
-	# def __init__(self, developer, task):
-	# 	self.developer = developer
- #    	self.task = task
- #    	self.progress = progress
-    	# self.task.update(progress)
 
 	def __unicode__(self):
 		return '{} {} {} {:.2f} {}'.format(self.developer.firstName, self.developer.lastName, 
